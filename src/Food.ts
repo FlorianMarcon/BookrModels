0717a0a6391995d06712629eeae0a789bd4ba07e
florian.marcon@epitech.eu <florian.marcon@epitech.eu@epitech.eu>
2020-03-21 09:21:15 +0100

    Food Create: - Create IUpdateFoodInput extending Partial ICreateFoodInput

--- src/Food.ts
+++ src/Food.ts
@@ -28,6 +28,18 @@ export interface ICreateFoodInput extends Partial<Food> {
 	idFoodGroup?	:	number;
 }
 
+export interface IUpdateFoodInput extends Partial<ICreateFoodInput> {
+	_id			:	number;
+
+	name?			:	string;	
+
+	price?			:	number;
+
+	description?	:	string;
+
+	idFoodGroup?	:	number;
+
+}
 export interface IFoodOptionInput {
 	onlyUncathegorized:	boolean;
 }
